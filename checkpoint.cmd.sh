bce4820fd689279867f16548430a0794b42fb96b
Anton Kozlov <akozlov@azul.com>
2024-01-12 17:25:48 +0200

    Simplify CRaC checkpoint command line

--- checkpoint.cmd.sh
+++ checkpoint.cmd.sh
@@ -5,17 +5,16 @@ detect_native_client() {
 }
 
 export AWS_NATIVE_CLIENT=$(detect_native_client /function/lib/jni/libaws-lambda-jni.*.so)
+export CRAC_CRIU_OPTS="--compress"
 
 # Ensure small PID, for privileged-less criu to be able to restore PID by bumping.
 # But not too small, to avoid clashes with other occasional processes on restore.
-exec /aws-lambda-rie /bin/bash -c '\
-	while [ 128 -ge $(cat /proc/sys/kernel/ns_last_pid) ]; do :; done; \
-	CRAC_CRIU_OPTS="--compress" \
-	setsid /jdk/bin/java \
+exec /aws-lambda-rie /jdk/bin/java \
 		-Xshare:off \
 		-XX:-UsePerfData \
+		-XX:CRaCMinPid=128 \
 		-XX:CRaCCheckpointTo=/cr \
 		-cp /function:/function/lib/* \
 		-Dcom.amazonaws.services.lambda.runtime.api.client.runtimeapi.NativeClient.JNI=$AWS_NATIVE_CLIENT \
 		--add-opens java.base/java.util=ALL-UNNAMED \
-		com.amazonaws.services.lambda.runtime.api.client.AWSLambda $0' "$@"
+		com.amazonaws.services.lambda.runtime.api.client.AWSLambda "$@"
